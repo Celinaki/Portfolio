2164ed392263de0ad36e4d4b83a20c4dd37d1276
Celinaki <celina1999@live.se>
2023-01-08 23:51:36 +0100

    fixed image in about page

--- src/pages/aboutme.jsx
+++ src/pages/aboutme.jsx
@@ -5,7 +5,7 @@ import Navbar from "../components/navbar"
 import * as aboutmestyle from "../cssmodules/aboutMe.module.scss"
 import Footer from '../components/footercomp.jsx'
 import Waver from "../components/waver";
-import SideImg from "../images/eugene-golovesov-VJ6aRAcrj-Y-unsplash.jpg"
+import SideImg from "../images/icy.jpg"
 
 
 const aboutMe = ({ data }) => {
@@ -18,8 +18,7 @@ const aboutMe = ({ data }) => {
 
           <div className={aboutmestyle.aboutwrapper}>
             <p className={aboutmestyle.firstp}>{node.presentation.presentation}</p>
-            <div className={aboutmestyle.imageholder} alt="Image with leafs"></div>
-            {/* <img src={SideImg} alt="" /> */}
+            <img src={SideImg} alt="Icy pattern" />
             <span className={aboutmestyle.lists}>
               <p>My job experiences {renderRichText(node.listOfJobs)}</p>
               <p>My education {renderRichText(node.listOfEducation)}</p>
@@ -41,8 +40,10 @@ export function Head() {
   return (
 
     <>
+    <meta></meta>
       <link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet"></link>
       <title>About me</title>
+      
     </>
   )
 
