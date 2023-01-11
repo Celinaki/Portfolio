ddf93c584b7ae9f939bfed0cf67afb5b4444264b
Celinaki <celina1999@live.se>
2023-01-11 21:50:39 +0100

    -

--- src/components/Footercomponent.jsx
+++ src/components/Footercomponent.jsx
@@ -4,25 +4,25 @@ import * as footerstyle from '../cssmodules/footer.module.scss'
 
 export default function Footercomp(){
   
-   const data = useStaticQuery(graphql`
-    query MyQuery {
-      contentfulFooter {
-        quote
-        title
-        publishedDate(formatString: "YYYY")
-      }
-    }
-    `)
+  //  const data = useStaticQuery(graphql`
+  //   query MyQuery {
+  //     contentfulFooter {
+  //       quote
+  //       title
+  //       publishedDate(formatString: "YYYY")
+  //     }
+  //   }
+  //   `)
 
     return (
         <div>
-            <footer >
+            {/* <footer >
               <span>
                 <p>By {data.contentfulFooter.title}</p>
 <p>Favorite quote {data.contentfulFooter.quote}</p>
               </span>
               <p>Copyright {data.contentfulFooter.publishedDate}</p> 
-            </footer>
+            </footer> */}
         </div>
     )
 }
